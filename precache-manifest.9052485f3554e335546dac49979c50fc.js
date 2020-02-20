f43a7fe958a386c19640d3adc39bf73062b1550d
Fredrik Lundkvist <flundkvi@kth.se>
2020-02-20 15:54:38 +0100

    Updates

---
+++ precache-manifest.9052485f3554e335546dac49979c50fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db94563d537594912a93ce632f171a4",
+    "url": "/ivis20-proj2/index.html"
+  },
+  {
+    "revision": "44dc4e6716d4f45745b6",
+    "url": "/ivis20-proj2/static/css/main.5f2f20db.chunk.css"
+  },
+  {
+    "revision": "ea6971affc7ca2afe93f",
+    "url": "/ivis20-proj2/static/js/2.f252543e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ivis20-proj2/static/js/2.f252543e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44dc4e6716d4f45745b6",
+    "url": "/ivis20-proj2/static/js/main.4f0f8d4e.chunk.js"
+  },
+  {
+    "revision": "aa2bd2bae7f239576e51",
+    "url": "/ivis20-proj2/static/js/runtime-main.e86f010b.js"
+  },
+  {
+    "revision": "4ec5f0550eead6add7da6888e508098d",
+    "url": "/ivis20-proj2/static/media/data.4ec5f055.csv"
+  },
+  {
+    "revision": "e82212c1b924fb5b1beb35e2d37332a4",
+    "url": "/ivis20-proj2/static/media/gdp_data.e82212c1.csv"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ivis20-proj2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "025403f09bb7ddbeecc9b905902f6e01",
+    "url": "/ivis20-proj2/static/media/world.025403f0.geojson"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8cc5f5a407b1adf9d4bd2862193e659.js"
+  "/ivis20-proj2/precache-manifest.9052485f3554e335546dac49979c50fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ivis20-proj2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
